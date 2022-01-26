bc61af31acd47ff42dc0d7e207cc48d64ef3d268
ngiann <ngiann@gmail.com>
2022-01-26 15:52:24 +0100

    updated plotting script

--- Mrk279_2019/Experiment1/plot_experiment1.jl
+++ Mrk279_2019/Experiment1/plot_experiment1.jl
@@ -1,20 +1,20 @@
-using MiscUtil, PyPlot, JLD2, Statistics, StatsFuns
+using MiscUtil, PyPlot, JLD2, Statistics, StatsFuns, ConvolvedGaussianProcesses, ADDatasets, TransferFunctions, Printf, LinearAlgebra
 
 function plot_experiment1()
 
     # Filenames
 
-    file_10_matern12 = "Mrk279_2019_physical_exp1_EF_10_matern12.jld2"
-    file_20_matern12 = "Mrk279_2019_physical_exp1_EF_20_matern12.jld2"
-    file_30_matern12 = "Mrk279_2019_physical_exp1_EF_30_matern12.jld2"
+    file_10_matern12 = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_10_matern12.jld2"
+    file_20_matern12 = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_20_matern12.jld2"
+    file_30_matern12 = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_30_matern12.jld2"
 
-    file_10_matern32 = "Mrk279_2019_physical_exp1_EF_10_matern32.jld2"
-    file_20_matern32 = "Mrk279_2019_physical_exp1_EF_20_matern32.jld2"
-    file_30_matern32 = "Mrk279_2019_physical_exp1_EF_30_matern32.jld2"
+    file_10_matern32 = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_10_matern32.jld2"
+    file_20_matern32 = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_20_matern32.jld2"
+    file_30_matern32 = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_30_matern32.jld2"
 
-    file_10_rbf = "Mrk279_2019_physical_exp1_EF_10_rbf.jld2"
-    file_20_rbf = "Mrk279_2019_physical_exp1_EF_20_rbf.jld2"
-    file_30_rbf = "Mrk279_2019_physical_exp1_EF_30_rbf.jld2"
+    file_10_rbf = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_10_rbf.jld2"
+    file_20_rbf = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_20_rbf.jld2"
+    file_30_rbf = "Mrk279_2019/Experiment1/Mrk279_2019_physical_exp1_EF_30_rbf.jld2"
 
     # Load saved results
 
@@ -79,6 +79,9 @@ function plot_experiment1()
 
     @show sum(p)
 
+    # Create horizontal bar plot for joint posterior probabilities
+    # of kernel and eddington fraction
+
     ax = figure(2); cla()
 
     labels = ["matern12,10%","matern12,20%","matern12,30%",
@@ -100,8 +103,73 @@ function plot_experiment1()
     hbars[8].set_color("g")
     hbars[9].set_color("b")
 
-    # ax.bar_label(hbars, labels=p,
-    #          padding=8, fontsize=14)
+    # Plot best fit for each combination of kernel and eddington fraction
+
+    ##################
+    # Matern 12, 10% #
+    ##################
+
+    bestmass_index = argmax(data_10_matern12["posterior"])
+    bestmass = masses[bestmass_index]
+    @printf("best mass for matern12, 10%% is %e\n", bestmass)
+
+    lambda, tobs, yobs, σobs = readdataset(; source = "Mrk279_2019");
+
+    tfarray = PhysicalTransferFunctions(mass = bestmass, eddingtonfraction = 10.0, wavelengths = lambda)
+
+    _, pred = convolvedgp(tarray=tobs, yarray=yobs, stdarray=σobs, kernelname="matern32", tfarray=tfarray, iterations=3000, numberofrestarts = 1, ρmax=10.0);
+
+    xtest = 57866:0.1:57957.0
+
+    μ, σ = pred(xtest)
+
+    figure()
+    title("Mrk279_2019, matern12, ef=10%")
+    clr = ["m", "b", "g", "r"]
+    for i in 1:4
+        plot(tobs[i], yobs[i], clr[i]*"o", markeredgecolor="k")
+
+        fill_between(xtest, μ[i] .+ σ[i], μ[i] .- σ[i], color=clr[i],  alpha=0.1)
+
+        plot(xtest, μ[i], "k-", linewidth=2, label = @sprintf("%d", lambda[i]))
+    end
+    # legend()
+
+    ax = axis()
+
+    ##################
+    # RBF, 20%       #
+    ##################
+
+    bestmass_index = argmax(data_20_rbf["posterior"])
+    bestmass = masses[bestmass_index]
+    @printf("best mass for rbf, 20%% is %e\n", bestmass)
+
+    lambda, tobs, yobs, σobs = readdataset(; source = "Mrk279_2019");
+
+    tfarray = PhysicalTransferFunctions(mass = bestmass, eddingtonfraction = 20.0, wavelengths = lambda)
+
+    _, pred = convolvedgp(tarray=tobs, yarray=yobs, stdarray=σobs, kernelname="rbf", tfarray=tfarray, iterations=3000, numberofrestarts = 1, ρmax=10.0);
+
+    xtest = 57866:0.1:57957.0
+
+    μ, Σ = pred(xtest)
+
+    figure()
+    title("Mrk279_2019, rbf, ef=20%")
+    clr = ["m", "b", "g", "r"]
+    for i in 1:4
+        plot(tobs[i], yobs[i], clr[i]*"o", markeredgecolor="k")
+
+        fill_between(xtest, μ[i] .+ σ[i], μ[i] .- σ[i], color=clr[i],  alpha=0.1)
+
+        plot(xtest, μ[i], "k-", linewidth=2, label = @sprintf("%d", lambda[i]))
+    end
+    # legend()
+
+    axis(ax)
+
+    #------------------------------------------
 
     return p
 end
