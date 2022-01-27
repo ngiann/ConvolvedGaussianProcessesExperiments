ab82d4776c909be03f300e0629953c62a44710fe
ngiann <ngiann@gmail.com>
2022-01-27 10:32:34 +0100

    add new synthetic experiment

---
+++ Synthetics/Experiment1/experiment1.jl
@@ -0,0 +1,36 @@
+# 27 Jan 2022
+#
+# Commands executed for getting results for synthetic data.
+#
+# Run on hephaistos
+#
+# ~/julia-1.7.1/bin/julia -O3 -64
+#
+
+@everywhere using ConvolvedGaussianProcesses, ProgressMeter, Suppressor
+
+using Printf, MiscUtil, ADDatasets, TransferFunctions, JLD2
+
+
+tobs, yobs, σobs, tfarray = simulatedatafromgp(N=50, Tmax=100, mass=2e8, σ=0.2);
+
+lambda = [4300.0; 5700.0; 6200.0; 7000.0]
+
+masses = collect(logrange(1e5, 1e10, 64));
+
+T = [PhysicalTransferFunctions(mass = m, eddingtonfraction = 10.0, wavelengths = lambda) for m in masses]
+
+
+# warmup
+
+for i in 1:2
+
+    outparallel = @showprogress pmap(tfarray->(@suppress performcv(tarray=tobs, yarray=yobs, stdarray=σobs, kernelname="matern32", tfarray=tfarray, iterations=2, numberofrestarts=1, ρmax=1.0)), T)
+
+end
+
+outparallel = @showprogress pmap(tfarray->(@suppress performcv(tarray=tobs, yarray=yobs, stdarray=σobs, kernelname="matern32", tfarray=tfarray, iterations=2500, numberofrestarts=1, ρmax=10.0)), T)
+
+prob = getprobabilities(outparallel)
+
+JLD2.save("experiment1.jld2", "out", outparallel, "prob", prob, "lambda", lambda, "masses", masses, "eddingtonfraction", 10.0, "N", 50, "Tmax", 100, "truemass"=2e8, "σ", 0.2)
