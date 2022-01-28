b4574fed3f53409471d17afd1173e76886466b9d
ngiann <ngiann@gmail.com>
2022-01-28 12:47:37 +0100

    Repeated experiment

--- Synthetics/Experiment2/experiment2.jl
+++ Synthetics/Experiment2/experiment2.jl
@@ -1,13 +1,13 @@
-# 27 Jan 2022
+# 28 Jan 2022
 #
 # Commands executed for getting results for synthetic data.
 #
 # Same as experiment1.jl but with more noise put on the data:
 # check σ argument when calling "simulatedatafromgp"
 #
-# Run on hephaistos
+# Run on laptop
 #
-# ~/julia-1.7.1/bin/julia -O3 -p64
+# ~/julia-1.7.1/bin/julia -O3 -p16
 #
 
 @everywhere using ConvolvedGaussianProcesses, ProgressMeter, Suppressor
@@ -16,6 +16,9 @@ using Printf, MiscUtil, ADDatasets, TransferFunctions, JLD2, UnicodePlots, PyPlo
 
 
 tobs, yobs, σobs, tfarray = simulatedatafromgp(N=50, Tmax=100, mass=2e8, σ=1.0);
+
+PyPlot.savefig("lightcurves.svg")
+PyPlot.savefig("lightcurves.png")
 
 lambda = [4300.0; 5700.0; 6200.0; 7000.0]
 
@@ -46,6 +49,41 @@ UnicodePlots.barplot(masses, prob)
 
 figure()
 plot(masses, prob, "o-"); xscale(:log)
+plot(2e8*ones(5), collect(LinRange(minimum(prob),maximum(prob), 5)), "r--")
 PyPlot.title("synthetic, true mass is 2e8")
 PyPlot.savefig("posteriormass.svg")
 PyPlot.savefig("posteriormass.png")
+
+
+# plot best fit
+
+figure()
+
+bestmass = masses[argmax(prob)]
+
+tfarray = PhysicalTransferFunctions(mass=bestmass, eddingtonfraction=10.0, wavelengths=lambda);
+
+fmin, pred = convolvedgp(tarray=tobs, yarray=yobs, stdarray=σobs, kernelname="matern32", iterations=10000, ρmax=10.0,  tfarray = tfarray);
+
+xtest = collect(LinRange(0.0, 101.0, 1000))
+
+μpred, σpred = pred(xtest)
+
+colours = ["b", "orange", "g", "r"]
+
+for i in 1:length(tobs)
+    plot(tobs[i], yobs[i], "o", color=colours[i], markeredgecolor="k")
+end
+
+for i in 1:length(tobs)
+    fill_between(xtest, μpred[i] .+ σpred[i], μpred[i] .- σpred[i], color=colours[i],  alpha=0.1)
+end
+
+for i in 1:length(tobs)
+    plot(xtest, μpred[i], color=colours[i])
+end
+
+PyPlot.title(@sprintf("best fit for mass=%e", bestmass))
+
+PyPlot.savefig("bestfit.svg")
+PyPlot.savefig("bestfit.png")
