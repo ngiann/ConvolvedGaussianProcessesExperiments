2ff53147d87e56394dec52cfa7539e94334a790b
ngiann <ngiann@gmail.com>
2022-02-01 09:40:15 +0100

    add experiment

---
+++ Mrk509/2017/Experiment1/runme.jl
@@ -0,0 +1,21 @@
+# Get results for Mrk509_2017
+
+# load data
+
+SOURCE = "Mrk509_2017"
+
+TF = PhysicalTransferFunctions
+
+lambda, tobs, yobs, σobs = readdataset(; source = SOURCE)
+
+# specify kernels to try
+K = ["matern12", "matern32", "rbf"]
+
+# specify eddington fractions to try
+E = [10.0, 20.0, 30.0]
+
+# make all combinations of K and E
+comb = vec(collect(Base.Iterators.product(K, E)))
+
+# run all experiments
+map(X -> runexperiment(transferFunctions = TF, lambda = lambda, tobs = tobs, yobs = yobs, σobs = σobs, objectname = SOURCE, kernelname =  X[1], ef = X[2]), comb);
