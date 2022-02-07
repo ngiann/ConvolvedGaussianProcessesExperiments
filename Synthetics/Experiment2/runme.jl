79c688d52a640e754145588e70b894cd9e263f3a
ngiann <ngiann@gmail.com>
2022-02-07 21:51:34 +0100

    fix sprintf error

--- Synthetics/Experiment2/runme.jl
+++ Synthetics/Experiment2/runme.jl
@@ -82,7 +82,7 @@ let
 
     subplot(211)
 
-    title(@sprintf("true mass is %e"), truemass)
+    title(@sprintf("true mass is %e", truemass))
 
     massmarginal = vec(sum(reshape(data["posterior"], size(TF)), dims=2))
 
@@ -90,7 +90,7 @@ let
 
     subplot(212)
 
-    title(@sprintf("true edfraction is %e"), trueedfrac)
+    title(@sprintf("true edfraction is %e", trueedfrac))
 
     accmarginal = vec(sum(reshape(data["posterior"], size(TF)), dims=1))
 
