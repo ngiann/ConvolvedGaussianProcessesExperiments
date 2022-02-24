f7380655a4715c61f69c8c91f9bebdfaf6309843
ngiann <ngiann@gmail.com>
2022-02-24 11:19:25 +0100

    add experiment

---
+++ Real/Mrk509/2016/Bands_2_3/runme.jl
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# SPECIFY DATA
+#-------------------------------------------------------------------------------
+
+SOURCE = "Mrk509_2016"
+
+
+# ❗ Same as experiment 1 but only two bands, the 2nd and 3rd❗
+
+lambda, tobs, yobs, σobs = readdataset(source = SOURCE)
+
+
+lambda, tobs, yobs, σobs = lambda[[2;3]], tobs[[2;3]], yobs[[2;3]], σobs[[2;3]]
+
+#-------------------------------------------------------------------------------
+# SPECIFY TRANSFER FUNCTIONS
+#-------------------------------------------------------------------------------
+
+# specify kernel
+kernelname = "matern32"
+
+# specify physical parameters
+masses     = collect(logrange(1e5, 1e10, 64))
+efractions = [1.0; 5.0; 10.0]
+
+# create combinations of transfer functions
+TF = pmap(((m,ef),) -> PhysicalTransferFunctionsEddington(mass = m, eddingtonfraction = ef, wavelengths = lambda), Iterators.product(masses, efractions))
+
+include("../../../../runmecommonpart.jl")
