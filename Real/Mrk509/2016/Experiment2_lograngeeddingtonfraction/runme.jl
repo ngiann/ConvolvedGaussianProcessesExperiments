b0d3ba815370f02b81dd2687885646453f45cdd8
ngiann <ngiann@gmail.com>
2022-02-25 20:34:21 +0100

    add experiment

---
+++ Real/Mrk509/2016/Experiment2_lograngeeddingtonfraction/runme.jl
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# SPECIFY DATA
+#-------------------------------------------------------------------------------
+
+SOURCE = "Mrk509_2016"
+
+
+lambda, tobs, yobs, σobs = readdataset(source = SOURCE)
+
+
+#-------------------------------------------------------------------------------
+# SPECIFY TRANSFER FUNCTIONS
+#-------------------------------------------------------------------------------
+
+# specify kernel
+kernelname = "matern32"
+
+# specify physical parameters
+masses     = collect(logrange(1e5, 1e10, 64))
+efractions = collect(logrange(1, 30, 10))
+
+# create combinations of transfer functions
+TF = pmap(((m,ef),) -> PhysicalTransferFunctionsEddington(mass = m, eddingtonfraction = ef, wavelengths = lambda), Iterators.product(masses, efractions))
+
+include("../../../../runmecommonpart.jl")
