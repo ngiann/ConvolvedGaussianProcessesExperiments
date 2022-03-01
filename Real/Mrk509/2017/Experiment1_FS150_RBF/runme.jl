fd78c5a2a43a2f96137b46d50c762891b0272e6d
ngiann <ngiann@gmail.com>
2022-03-01 15:14:21 +0100

    minor change

--- Real/Mrk509/2017/Experiment1_FS150_RBF/runme.jl
+++ Real/Mrk509/2017/Experiment1_FS150_RBF/runme.jl
@@ -17,7 +17,7 @@ kernelname = "rbf"
 
 # specify physical parameters
 masses     = collect(logrange(1e5, 1e10, 64))
-efractions = [1.0; 5.0; 10.0]
+efractions = [10.0]
 
 # create combinations of transfer functions
 TF = pmap(((m,ef),) -> PhysicalTransferFunctionsEddington(mass = m, eddingtonfraction = ef, wavelengths = lambda), Iterators.product(masses, efractions))
