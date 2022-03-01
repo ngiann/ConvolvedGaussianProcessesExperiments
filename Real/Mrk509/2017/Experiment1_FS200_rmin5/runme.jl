2c060d5945b1d4cd672d1f227d37780db03adc51
ngiann <ngiann@gmail.com>
2022-03-01 11:28:10 +0100

    add experiment

---
+++ Real/Mrk509/2017/Experiment1_FS200_rmin5/runme.jl
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# SPECIFY DATA
+#-------------------------------------------------------------------------------
+
+SOURCE = "Mrk509_2017"
+
+
+lambda, tobs, yobs, σobs = readdataset(source = SOURCE)
+
+
+#-------------------------------------------------------------------------------
+# SPECIFY TRANSFER FUNCTIONS
+#-------------------------------------------------------------------------------
+
+# specify kernel
+kernelname = "matern32"
+
+# specify physical parameters
+masses     = collect(logrange(1e5, 1e10, 64))
+efractions = [1.0; 5.0; 10.0]
+
+# create combinations of transfer functions
+TF = pmap(((m,ef),) -> PhysicalTransferFunctionsEddington(mass = m, eddingtonfraction = ef, wavelengths = lambda), Iterators.product(masses, efractions))
+
+ρmin = 5.0
+
+include("../../../../runmecommonpart.jl")
