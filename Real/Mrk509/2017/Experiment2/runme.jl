3d6daa766fe17fdc6bf93de053deb8abe99cdbb6
Nikos Gianniotis <giannins@hitssv565.villa-bosch.de>
2022-03-04 10:22:57 +0100

    add results

---
+++ Real/Mrk509/2017/Experiment2/runme.jl
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# SPECIFY DATA
+#-------------------------------------------------------------------------------
+
+SOURCE = "Mrk509_2017"
+
+
+lambda, tobs, yobs, σobs = readdataset(source = SOURCE)
+
+
+#-------------------------------------------------------------------------------
+# SPECIFY TRANSFER FUNCTIONS
+#-------------------------------------------------------------------------------
+
+# specify kernel
+kernelname = "matern32"
+
+# specify physical parameters
+masses     = collect(logrange(1e5, 1e10, 64))
+efractions = collect(logrange(1.0, 50, 10))# 5.0; 10.0]
+
+# create combinations of transfer functions
+TF = pmap(((m,ef),) -> PhysicalTransferFunctionsEddington(mass = m, eddingtonfraction = ef, wavelengths = lambda), Iterators.product(masses, efractions))
+
+FS = 175
+
+ρmin = 0.5
+
+include("../../../../runmecommonpart.jl")
