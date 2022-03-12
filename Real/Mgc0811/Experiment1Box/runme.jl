d3b0b966b5dbe60162d0f66e7167308818953434
ngiann <ngiann@gmail.com>
2022-03-12 19:53:43 +0100

    add experiment

---
+++ Real/Mgc0811/Experiment1Box/runme.jl
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# SPECIFY DATA
+#-------------------------------------------------------------------------------
+
+SOURCE = "Mgc0811"
+
+
+lambda, tobs, yobs, σobs = readdataset(source = SOURCE)
+
+
+#-------------------------------------------------------------------------------
+# SPECIFY TRANSFER FUNCTIONS
+#-------------------------------------------------------------------------------
+
+# specify kernel
+kernelname = "matern32"
+
+# specify physical parameters
+masses     = collect(logrange(1e5, 1e10, 64))
+efractions = [1.0; 5.0; 10.0]
+
+# create combinations of transfer functions
+
+TF = pmap(((m,ef),) -> ShakuraBoxTransferFunctionsEddington(mass = m, eddingtonfraction = ef, wavelengths = lambda), Iterators.product(masses, efractions))
+
+FS = 200
+
+ρmin = 0.1
+
+include("../../../runmecommonpart.jl")
